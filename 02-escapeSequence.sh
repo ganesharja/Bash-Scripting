9f112689e223365d0fe6dac695e6f20cfc9aa263
Ganesh Arja <jayaganesharja.com>
2023-08-14 16:22:34 +0530

    Auto push

---
+++ 02-escapeSequence.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+# Shebang Notation : This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+echo Welcome To Bach Scripting 
+
+# This is how we can execute the scripts
+# $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
+
+#  \n : This indicates a new line 
+# \t  : This gives a tab space 
+ Ganesh\nDevOps
+#  Ganesh
+#  DevOps
+
+echo Line1
+echo Line2
+echo Line3
+echo Line4
+
+echo -e "Line5\nLine6"
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
+
+echo -e "Cloud\tDevOps\tare\tBooming"
+echo -e "We\tAre\tLearning\tCloudDevOps \n\tThis is my batch55"
